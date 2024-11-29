fe3162cd664cf6e76ae0bcb604a4c82eb0b8dc4c
shimdevkun <shimdevkun@gmail.com>
2024-11-29 16:05:03 -0500

    Implement score on task completion

--- app/src/main/java/com/pawgress/model/UserPet.java
+++ app/src/main/java/com/pawgress/model/UserPet.java
@@ -2,34 +2,14 @@ package com.pawgress.model;
 
 public class UserPet {
     private String name;
-    private int imageResId;
-    private int health;
-    private int happiness;
-    private int satiety;
+    private PetType petType;
 
-    public int getHappiness() {
-        return happiness;
-    }
-
-    public void setHappiness(int happiness) {
-        this.happiness = happiness;
-    }
-
-    public int getHealth() {
-        return health;
-    }
-
-    public void setHealth(int health) {
-        this.health = health;
-    }
-
-    public int getImageResId() {
-        return imageResId;
-    }
-
-    public void setImageResId(int imageResId) {
-        this.imageResId = imageResId;
-    }
+    /**
+     * The stats numbers represent percentages (i.e., 50 = 50%)
+     */
+    private int health = 50;
+    private int happiness = 50;
+    private int satiety = 30;
 
     public String getName() {
         return name;
@@ -39,11 +19,60 @@ public class UserPet {
         this.name = name;
     }
 
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = Math.min(100, health);
+    }
+    public int getHappiness() {
+        return happiness;
+    }
+
+    public void setHappiness(int happiness) {
+        this.happiness = Math.min(100, happiness);
+    }
+
     public int getSatiety() {
         return satiety;
     }
 
     public void setSatiety(int satiety) {
-        this.satiety = satiety;
+        this.satiety = Math.min(100, satiety);
+    }
+
+    public void updateStat(PetStat stat, int points) {
+        switch (stat) {
+            case HEALTH:
+                setHealth(this.health + points);
+                break;
+            case HAPPINESS:
+                setHappiness(this.happiness + points);
+                break;
+            case SATIETY:
+                setSatiety(this.satiety + points);
+                break;
+        }
+    }
+
+    public int getStat(PetStat stat) {
+        switch (stat) {
+            case HEALTH:
+                return this.health;
+            case HAPPINESS:
+                return this.happiness;
+            case SATIETY:
+                return this.satiety;
+        }
+        return 0;
     }
 }
